361cf183c49c9da71930cb43caa23105f4d2fa50
hadan16 <YOUR_GITHUB_EMAIL_ADDRESS>
2017-03-14 12:17:47 -0700

    Adding pages

---
+++ public/scripts/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+  //slick image carousel
+  'use strict';
+  $('.your-class').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    swipeToSlide: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  //smooth scrolling
+  $(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - $('nav').outerHeight()
+    }, 500);
+  });
+
+
+
+
+});
